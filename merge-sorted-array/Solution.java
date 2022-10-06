a67c52830e95feff63ccc65bb1b11d92a27e2554
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2022-10-06 19:48:05 +0530

    forgot to save file in previous commit merge-sorted-array

--- merge-sorted-array/Solution.java
+++ merge-sorted-array/Solution.java
@@ -1,8 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if ( nums2[0] > nums1[m-1] ) {
-            int j = m , k = m+n ;
-            for (  ) 
-        }
+        int[] copiedArray = new int[ m ] ;
+            for ( int i = 0 ; i < m ; i ++ ) copiedArray[i] = nums1[i] ;
+            int i = 0 , j = 0 , k = 0 ;
+            while ( i < m && j < n ) {
+                nums1[k++] = ( copiedArray[i] < nums2[j] ) ? copiedArray[i++] : nums2[j++] ;
+            }
+            while ( i < m ) nums1[k++] = copiedArray[i++] ;
+            while ( j < n ) nums1[k++] = nums2[j++] ;
     }
 }
