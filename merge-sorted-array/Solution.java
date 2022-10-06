0a02aaed2585d8d0d419f4233ac380ca4c11d4ea
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2022-10-06 19:44:39 +0530

    merge-sorted-array

---
+++ merge-sorted-array/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if ( nums2[0] > nums1[m-1] ) {
+            int j = m , k = m+n ;
+            for (  ) 
+        }
+    }
+}
