2284585b4eed69c64314642a88eb46f14ee04287
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2022-10-07 05:47:04 +0530

    balanced-binary-tree

---
+++ balanced-binary-tree/Solution.java
@@ -0,0 +1,22 @@
+import java.util.logging.LogRecord;
+
+class Solution {
+
+    boolean bal = true ;
+
+    public boolean isBalanced( TreeNode root ) {
+        check( root ) ;
+        return bal ;
+    }
+
+    public int check( TreeNode node ) {
+        if ( node == null ) return 0 ;
+        else if ( node.left == null && node.right == null ) return 1 ;
+        else {
+            int  l = check(node.left) , r = check(node.right) ;
+            if ( l-r > 1 || l-r < -1 ) bal = false ;
+            return 1 + (( l > r ) ? l : r) ;
+        }
+    }
+
+}
