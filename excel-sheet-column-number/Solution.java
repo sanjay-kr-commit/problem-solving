10e5521e324f61411cecae605bc6c46d2460fedc
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2022-10-19 11:11:30 +0530

    excel-sheet-column-number

---
+++ excel-sheet-column-number/Solution.java
@@ -0,0 +1,11 @@
+// 2ms
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int num = 0 ;
+        for ( int i = 0 ; i < columnTitle.length() ; i++ ) {
+            num *= 26 ;
+            num += columnTitle.charAt(i) - '@' ;
+        }
+        return num ;
+    }
+}
