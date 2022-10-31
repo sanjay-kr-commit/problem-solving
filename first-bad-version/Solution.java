b0a7edba764eb89cb311c601dce5f26820d4b588
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2022-10-31 10:13:29 +0530

    better algo

--- first-bad-version/Solution.java
+++ first-bad-version/Solution.java
@@ -16,3 +16,16 @@
             return (isBadVersion( l )&&l<bad)?l:bad ;
         }
     }
+
+    // 17 ms
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int l = 1 , m , r = n ;
+            while ( l <= r ) {
+                m = l + ( r - l  ) / 2 ;
+                if ( isBadVersion( m ) == false ) l = m + 1 ;
+                else r = m-1 ;
+            }
+            return l ;
+        }
+    }
