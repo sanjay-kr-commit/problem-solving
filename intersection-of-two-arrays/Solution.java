6f461353b2d46f164f12996619edf80cda0beab0
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-01-06 23:58:44 +0530

    better approach

--- intersection-of-two-arrays/Solution.java
+++ intersection-of-two-arrays/Solution.java
@@ -1,14 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-      HashSet<Integer> set = new HashSet<>() ;
-      for ( int i = 0 ; i < nums1.length ; i++ ) set.add( nums1[i] ) ;
-      ArrayList<Integer> unique = new ArrayList<>() ;
-      for ( int i = 0 ; i < nums2.length ; i++ ) {
-        if ( set.contains( nums2[i] ) && !unique.contains( nums2[i] ) ) unique.add( nums2[i] ) ;
-      }
-      return unique.stream().mapToInt(i -> i).toArray() ;
+        boolean [] map = new boolean[1001] ;
+        for ( int i = 0 ; i < nums1.length ; i++ ) map[nums1[i]] = true ;
+        ArrayList<Integer> unique = new ArrayList<>() ;
+        for ( int i = 0 ; i < nums2.length ; i++ ){
+            if ( map[nums2[i]] ) {
+                unique.add( nums2[i] ) ;
+                map[nums2[i]] = false ;
+            }
+        }
+        return unique.stream().mapToInt(i -> i).toArray();
     }
 }
