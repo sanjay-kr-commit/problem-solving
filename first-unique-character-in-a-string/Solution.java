7240b2c45070d29123b7b64d3546fd9084c8c580
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-01-07 13:34:30 +0530

    first-unique-character-in-a-string

---
+++ first-unique-character-in-a-string/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int firstUniqChar(String s) {
+      int[] count = new int[26] ;
+      char[] ss = s.toCharArray() ;
+      for ( int i = 0 ; i < ss.length ; i++ ) count[ss[i]-'a']++ ;
+      for ( int i = 0 ; i < ss.length ; i++ ) if ( count[ss[i]-'a'] == 1 ) return i ;
+      return -1 ;  
+    }
+}
