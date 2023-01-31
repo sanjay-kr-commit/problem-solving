058f82ab62f745ce943e54715da9a1ca26fedd86
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-01-31 11:21:14 +0530

    next-greater-element

---
+++ next-greater-element-i/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    int ans[] = new int[nums1.length] ;
+    HashMap<Integer,Integer> map = new HashMap<>() ;
+    for ( int i = 0 ; i < nums2.length ; i++ ) map.put( nums2[i] , i ) ;
+    for ( int i = 0 ; i < nums1.length ; i++ ) {
+      ans[i] = -1 ;
+      int index = ( map.get( nums1[i] ) != null ) ? map.get( nums1[i] ) + 1 : -1 ;
+      if ( index == -1 || index >= nums2.length ) continue ;
+      while ( index < nums2.length ) {
+        if ( nums2[index] > nums1[i] ) {
+          ans[i] = nums2[index] ;
+          break ;
+        }
+        index++ ;
+      }
+    }
+    return ans ;
+  }
+}
