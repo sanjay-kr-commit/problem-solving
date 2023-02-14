1a8664a7d3e2851caa5ddf80d04822c4f33f6525
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-02-15 02:13:20 +0530

    clean code

--- relative-ranks/Solution.java
+++ relative-ranks/Solution.java
@@ -1,29 +1,28 @@
 class Solution {
+
   public String[] findRelativeRanks( int [] score ) {
-    String formattedRank[] = new String[score.length] ;
-    HashMap<Integer,String> map = new HashMap<>() ;
-    PriorityQueue<Integer> rank = new PriorityQueue<>( Collections.reverseOrder() ) ;
-    for ( int i : score ) rank.add( i ) ;
+
+    String[] formattedRank = new String[ score.length ] ;
+    
+    PriorityQueue<Integer> que = new PriorityQueue<>( Collections.reverseOrder() ) ;
+    
+    for ( int i = 0 ; i < score.length ; i++ ) que.add( score[i] ) ;
+    
+    HashMap<Integer,String> mappedRank = new HashMap<>() ;
+
     int index = 1 ;
-    while ( !rank.isEmpty() ) {
-      switch ( index ) {
-        case 1:
-        map.put( rank.remove() , "Gold Medal" ) ;
-        break;
-        case 2:
-        map.put( rank.remove() , "Silver Medal" ) ;
-        break;
-        case 3:
-        map.put( rank.remove() , "Bronze Medal" ) ;
-        break;
-        default:
-        map.put( rank.remove() , Integer.toString( index ) ) ;
-        break;
-      }
-      index++ ;
+    while ( !que.isEmpty() ) mappedRank.put( que.remove() , Integer.toString(index++) ) ;
+
+    for ( int i = 0 ; i < score.length ; i++ ) {
+      int rank = mappedRank.get( score[i] ) ;
+      if ( rank == 1 ) formattedRank[i] = "Gold Medal" ;
+      else if ( rank == 2 ) formattedRank[i] = "Silver Medal" ;
+      else if ( rank == 3 ) formattedRank[i] = "Bronze Medal" ;
+      else formattedRank[i] = Integer.toString(rank) ;
     }
-    index = 0 ;
-    for ( int i : score ) formattedRank[index++] = map.get(i) ;
+
     return formattedRank ;
+
   }
+
 }
