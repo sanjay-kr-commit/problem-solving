995a5c592ae1c8a640cd73444c3090e35cc50293
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-05-07 15:03:53 +0530

    optimise ans average of level of binary tree

--- average-of-levels-in-binary-tree/Solution.java
+++ average-of-levels-in-binary-tree/Solution.java
@@ -13,6 +13,53 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// 1ms
+class Solution {
+
+  ArrayList<double[]> state = new ArrayList<>() ;
+
+  public List<Double> averageOfLevels(TreeNode root) {
+    gatherLevelSum( root , 0 ) ;
+    return average() ;
+  }
+
+  private void gatherLevelSum( TreeNode node , int level ) {
+    if ( node == null ) return ;
+    if ( state.size()-1 < level ) {
+       state.add( level , new double[2] ) ;
+    }
+    state.get( level )[0] += 1 ;
+    state.get( level )[1] += node.val ;
+    gatherLevelSum( node.left , level + 1 ) ;
+    gatherLevelSum( node.right , level + 1 ) ;
+  }
+
+  private List<Double> average() {
+    ArrayList<Double> avg = new ArrayList<>() ;
+    for ( double[] i : state ) {
+      avg.add( i[1] / i[0] ) ;
+    }
+    return avg ;
+  }
+
+}
+
+// 2m HashMap isn't very spcace effiecient and also take quiet a long time to initialise compared to array list
 class Solution {
 
   Map<Integer,double[]> count = new HashMap<>() ;
