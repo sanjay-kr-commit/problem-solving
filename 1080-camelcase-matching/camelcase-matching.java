7521fa7f7c8a3e3314a64e3e331d3d681aac6a47
sanjay <sanjay.kr.on@gmail.com>
2024-03-30 14:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (81.26%) - LeetSync

--- 1080-camelcase-matching/camelcase-matching.java
+++ 1080-camelcase-matching/camelcase-matching.java
@@ -1,35 +1,25 @@
 class Solution {
     public List<Boolean> camelMatch(String[] queries, String pattern) {
-        ArrayList<Boolean> checks = new ArrayList<>( queries.length ) ;
-        if (pattern.isEmpty() || queries.length < 1 ) return checks ;
-        int len = pattern.length() ;
-        outer:
-        for ( String query : queries ) {
-            int i = 0 , j = 0 ,
-                len2 = query.length() ;
-            while ( i < len && j < len2 ) {
-                char chp = pattern.charAt( i ) ;
-                char chq = query.charAt( j ) ;
-                if ( !isCapital(chp) && isCapital(chq) || (isCapital(chp)&&isCapital(chq)&&chq != chp) ) {
-                    checks.add( false ) ;
-                    continue outer;
-                } if ( chp == chq ) i++ ;
-                j++ ;
-            }
-            if ( i >= len ) while ( j < len2  ) {
-                if ( isCapital( query.charAt(j) ) ) {
-                    checks.add( false ) ;
-                    continue outer;
+        List<Boolean> list = new ArrayList<>();
+
+        for (String s: queries) {
+            int index = 0;
+            boolean ans = true;
+            for (char c: s.toCharArray()) {
+                if (index < pattern.length() && pattern.charAt(index) == c) {
+                    index++;
+                    continue;
                 }
-                j++;
+                if (c >= 60 && c <= 85) {
+                    if (index >= pattern.length() || c != pattern.charAt(index)) {
+                        ans = false;
+                        break;
+                    }
+                }
             }
-            checks.add( i >= len ) ;
+            if (index < pattern.length()) ans = false;
+            list.add(ans);
         }
-        return checks ;
+        return list;
     }
-
-    boolean isCapital( char c ) {
-        return c >= 'A' && c <= 'Z' ;
-    }
-
 }
