e039266521615bc9660eddc04fd3011a27599865
sanjay <sanjay.kr.on@gmail.com>
2024-03-31 14:35:00 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (49.13%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>() ;
-        for ( int i : asteroids ) {
-            boolean isNegative = i < 0 ;
-            int val = i * ((isNegative) ? -1 : 1) ;
-            if (!isNegative || st.isEmpty() || st.peek() < 0) st.push( i ) ;
+        int [] collisionStats = new int[ asteroids.length ] ;
+        int size = -1 ;
+        for ( int asteroid : asteroids ) {
+            boolean isNegative = asteroid < 0 ;
+            int val = asteroid * ((isNegative)?-1:1) ;
+            if ( !isNegative || size == -1 || collisionStats[size] < 0 ) collisionStats[++size] = asteroid ;
             else {
-                while ( !st.isEmpty() && st.peek() > 0 && st.peek() < val ) st.pop() ;
-                if ( !st.isEmpty() && st.peek() == val ) st.pop() ;
-                else if ( st.isEmpty() || st.peek() < 0 || st.peek() < val ) st.push( i ) ;
+                while ( size != -1 && collisionStats[size] > 0 && collisionStats[size] < val ) size-- ;
+                if ( size != -1 && collisionStats[size] == val ) size-- ;
+                else if ( size == -1 || collisionStats[size] < 0 || collisionStats[size] < val ) collisionStats[++size] = asteroid ;
             }
         }
-        int [] arr = new int[ st.size() ] ;
-        for ( int i = 0 ; !st.isEmpty() ; i++ ) arr[i] = st.pop() ;
-        for ( int left = 0 ; left < arr.length/2 ; left++ ) {
-            int right = arr.length-left-1;
-            arr[left] += arr[right] ;
-            arr[right] = arr[left] - arr[right] ;
-            arr[left] -= arr[right] ;
-        }
-        return arr ;
+        size++ ;
+        int [] collisionImpact = new int[ size ] ;
+        System.arraycopy(collisionStats, 0, collisionImpact, 0, size);
+        return collisionImpact ;
     }
 }
