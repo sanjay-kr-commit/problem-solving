38d41518123c676e15db0c568187900ba4747f6e
sanjay <sanjay.kr.on@gmail.com>
2024-04-19 01:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (5.06%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public Node copyRandomList(Node head) {
+        if ( head == null ) return null ;
+        Node dummy = new Node(0) ;
+        int size = cloneList( head , dummy ) ;
+        addRandomPointer( head , dummy.next , size );
+        return dummy.next ;
+    }
+
+    int cloneList(Node head , Node clone ) {
+        int size = 0 ;
+        while ( head != null ) {
+            clone.next = new Node(head.val) ;
+            head = head.next ;
+            clone = clone.next ;
+            size++ ;
+        }
+        return size ;
+    }
+
+    void addRandomPointer(Node head , Node clone , int size ) {
+        int [] storeValue = new int[ size ] ;
+        Node[] clonePointer = new Node[ size ] ;
+        int i = 0 ;
+        Node headClone = head , cloneClone = clone ;
+        while ( i < size ) {
+            storeValue[i] = headClone.val ;
+            headClone.val = i ;
+            clonePointer[i] = cloneClone ;
+            headClone = headClone.next ;
+            cloneClone = cloneClone.next ;
+            i++ ;
+        }
+        headClone = head ;
+        cloneClone = clone ;
+        while ( headClone != null ) {
+            if ( headClone.random != null ) cloneClone.random = clonePointer[headClone.random.val] ;
+            headClone = headClone.next ;
+            cloneClone = cloneClone.next ;
+        }
+        headClone = head ;
+        while ( headClone != null ) {
+            headClone.val = storeValue[headClone.val] ;
+            headClone = headClone.next ;
+        }
+
+    }
+
+}
