3dc47fbdfdb3a94be24e719315a3105407a06cb2
sanjay <sanjay.kr.on@gmail.com>
2024-05-20 01:33:03 +0530

    Time: 1 ms (98.71%) | Memory: 41.5 MB (42.40%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,16 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> scopeStack = new Stack<>() ;
-        for (int i = 0; i < s.length(); i++) {
+        char[] stack = new char[s.length()];
+        int pointer = 0 ;
+        for ( int i = 0 ; i < s.length() ; i++ ) {
             char c = s.charAt(i);
-            if ( c == '(' || c == '{' || c == '[' ) scopeStack.push( c ) ;
-            else if ( c == ')' || c == '}' || c == ']' ) {
-                // no opening pair found
-                if ( scopeStack.isEmpty() ) return false ;
-                char openPair = scopeStack.pop();
-                // pair mismatch
-                if ( c == ')' && openPair != '(') return false;
-                if ( c == '}' && openPair != '{' ) return false;
-                if ( c == ']' && openPair != '[' ) return false;
+            if ( c == '(' || c == '[' || c == '{' ) stack[pointer++] = c;
+            else if ( pointer == 0 ) return false ;
+            else {
+                char openingPair = stack[--pointer];
+                if ( openingPair == '(' && c != ')' || openingPair == '[' && c != ']' || openingPair == '{' && c != '}' ) return false ;
             }
         }
-        return scopeStack.empty() ;
+        return pointer == 0 ;
     }
 }
