8dd3f7d5bfb6ef17dc1ae2d2aca0d61533c7dd3a
sanjay <sanjay.kr.on@gmail.com>
2024-05-20 01:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (89.35%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for ( int i = 0 ; i < nums.length ; i++ ) {
-            int x = target - nums[i] ;
-            for ( int j = 0 ; j < nums.length ; j++ ) {
-                if ( j == i ) continue;
-                if ( x == nums[j] ) return new int [] {i , j } ;
+        // s and e represent start and end of subarray of nums
+        for ( int s = 0 , e = nums.length-1 ; s < nums.length/2 ; s++ , e-- ) {
+            // inside this subarray we'll search for remaining number
+            // but cycles are more expensive than compare
+            // we'll compare element from both direction 
+            // and create an offset of one from subarray in order to avoid
+            // unnecessary checks
+            for ( int ss = s+1 , se = e-1 ; ss < nums.length-s ; ss++ , se-- ) {
+                if ( nums[ss] + nums[s] == target ) return new int[] { s , ss } ;
+                if ( nums[e] + nums[se] == target ) return new int[] { e , se } ;
             }
+
         }
-        return new int[2] ;
+        return new int[]{} ;
     }
 }
