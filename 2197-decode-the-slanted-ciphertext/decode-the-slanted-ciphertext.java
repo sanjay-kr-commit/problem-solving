6f73cec62186fe61f58f65572e9305c53194a144
sanjay <sanjay.kr.on@gmail.com>
2024-05-27 17:14:34 +0530

    Time: 8 ms (97.41%) | Memory: 55.2 MB (43.10%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,18 +1,21 @@
 class Solution {
     public String decodeCiphertext(String encodedText, int rows) {
-        if ( rows == 1 || encodedText.isEmpty()) return encodedText ;
-        StringBuilder decodedString = new StringBuilder() ;
-        int n = encodedText.length() , cols = n / rows , colPointer = 0 , rowPointer = 0 ;
-        while ( rowPointer < cols ) {
-            decodedString.append( encodedText.charAt(colPointer) ) ;
-            colPointer += cols + 1 ;
-            if ( colPointer >= n ) {
-                rowPointer++ ;
-                colPointer = rowPointer ;
+        if (rows == 1 || encodedText.isEmpty() ) return encodedText;
+        int len = encodedText.length();
+        int col = len/rows;
+        char[] result = new char[len];
+        int resultInd = 0;
+        for(int i = 0; i < col;i++) {
+            int index = i;
+            int x = Math.min(rows, col-i);
+            for(int j = 0; j < x ; j++) {
+                result[resultInd++] = encodedText.charAt(index);
+                index+=col+1;
             }
+
         }
-        int spaces = decodedString.length()-1 ;
-        while ( spaces > 0 && decodedString.charAt(spaces) == ' ' ) spaces-- ;
-        return decodedString.substring( 0 , spaces+1 ) ;
+        int j = len-1;
+        while(result[j] == ' ' || result[j] == 0) j--;
+        return String.copyValueOf(result, 0, j+1);
     }
 }
