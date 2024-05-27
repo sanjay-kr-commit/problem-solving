8ac20930b40040d9197d5ded1ffdf1d04e29bd20
sanjay <sanjay.kr.on@gmail.com>
2024-05-27 08:28:10 +0530

    Time: 58 ms (13.64%) | Memory: 45.3 MB (86.36%) - LeetSync

---
+++ 1804-maximum-binary-string-after-change/maximum-binary-string-after-change.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String maximumBinaryString(String s) {
+        int ones = 0, zeros = 0, n = s.length();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < n; ++i) {
+            res.append( '1' ) ;
+            if (s.charAt(i) == '0')
+                zeros++;
+            else if (zeros == 0)
+                ones++;
+        }
+        if (ones < n)
+            res.setCharAt(ones + zeros - 1, '0');
+        return res.toString();
+    }
+}
