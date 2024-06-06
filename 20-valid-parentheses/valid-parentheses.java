05f59d68dd13011010e7baeed1bcd66b7166f03b
sanjay <sanjay.kr.on@gmail.com>
2024-06-06 17:12:20 +0530

    Time: 1 ms (98.87%) | Memory: 41.4 MB (57.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,10 +5,7 @@ class Solution {
         for ( int i = 0 ; i < s.length() ; i++ ) {
             char c = s.charAt(i);
             if ( c == '(' || c == '{' || c == '[' ) stack[++pointer] = c ;
-            else if ( pointer == -1 ) return false ;
-            else if ( c == ')' && stack[pointer--] != '(' ) return false ;
-            else if ( c == ']' && stack[pointer--] != '[' ) return false ;
-            else if ( c == '}' && stack[pointer--] != '{' ) return false ;
+            else if ( pointer == -1 || ( c == ')' && stack[pointer--] != '(' ) ||  ( c == ']' && stack[pointer--] != '[' ) || ( c == '}' && stack[pointer--] != '{' ) ) return false ;
         }
         return pointer == -1 ;
     }
