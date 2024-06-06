efa6a813a8464aa3464739e674d4fc007660f1ee
sanjay <sanjay.kr.on@gmail.com>
2024-06-06 17:11:16 +0530

    Time: 1 ms (98.87%) | Memory: 41.2 MB (72.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
-        char[] stack = new char[s.length()];
-        int pointer = 0 ;
+        char [] stack = new char[s.length()];
+        int pointer = -1 ;
         for ( int i = 0 ; i < s.length() ; i++ ) {
             char c = s.charAt(i);
-            if ( c == '(' || c == '[' || c == '{' ) stack[pointer++] = c;
-            else if ( pointer == 0 ) return false ;
-            else {
-                char openingPair = stack[--pointer];
-                if ( openingPair == '(' && c != ')' || openingPair == '[' && c != ']' || openingPair == '{' && c != '}' ) return false ;
-            }
+            if ( c == '(' || c == '{' || c == '[' ) stack[++pointer] = c ;
+            else if ( pointer == -1 ) return false ;
+            else if ( c == ')' && stack[pointer--] != '(' ) return false ;
+            else if ( c == ']' && stack[pointer--] != '[' ) return false ;
+            else if ( c == '}' && stack[pointer--] != '{' ) return false ;
         }
-        return pointer == 0 ;
+        return pointer == -1 ;
     }
 }
