279ea1e02e8715129f51842db525d3705286f58c
sanjay <sanjay.kr.on@gmail.com>
2024-08-04 11:43:51 +0530

    Time: 225 ms (8.11%) | Memory: 100.8 MB (5.40%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,22 +1,37 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int mod = 1000000007 ;
+
+        LinkedList<Integer> list = new LinkedList<>();
+
         for ( int i = 0 ; i < n ; i++ ) {
             int sum = 0 ;
             for ( int j = i ; j < n ; j++ ) {
                 sum += nums[j];
-                pq.add( sum ) ;
+                list.add( sum ) ;
             }
         }
 
-        for ( int i = 1 ; i < left ; i++ ) pq.poll() ;
-        int sum = 0 ;
-        for ( int i = left ; i <= right && pq.peek() != null ; i++ ) {
-            sum = ((sum +pq.poll())  % 1000000007);
+        Collections.sort( list );
+
+        int i = 0 ;
+        Iterator<Integer> iterator = list.iterator() ;
+        while ( iterator.hasNext() && i < left-1 ) {
+            i++ ;
+            iterator.next() ;
         }
+        int sum = 0 ;
+        while ( iterator.hasNext() && i < right ) {
+            i++ ;
+            sum = ((sum +iterator.next()) % mod );
+        }
+
         return sum ;
     }
 }
