4b762c2ad2dd91e84aad370b4a60aa06d77375bf
sanjay <sanjay.kr.on@gmail.com>
2024-08-09 22:17:50 +0530

    Time: 6 ms (8.74%) | Memory: 45.5 MB (7.40%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,13 +1,14 @@
 class Solution {
 
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        List<int[]> resultList = new ArrayList<>( rows * cols + 1 );
+        int[][] result = new int[ rows * cols ][2];
         int size = rows * cols ;
         int rEnd = rStart , cEnd = cStart ;
-        int i = rStart , j = cStart , x = 0 , y = 1 ;
-        while ( resultList.size() < size ) {
+        int i = rStart , j = cStart , x = 0 , y = 1 , index = 0 ;
+        while ( index < size ) {
             if ( i < rows && i > -1 && j < cols && j > -1 ) {
-                resultList.add( new int[]{ i , j } );
+                result[index][0] = i ;
+                result[index++][1] = j ;
             }
             if ( i == rStart && j == cEnd+1 ) {
                 x++ ; y-- ;
@@ -22,8 +23,6 @@ class Solution {
             }
             i += x ; j += y ;
         }
-        int [][] result = new int[resultList.size()][2];
-        for ( int k = 0 ; k < resultList.size() ; k++ ) result[k] = resultList.get(k) ;
         return result ;
     }
 
