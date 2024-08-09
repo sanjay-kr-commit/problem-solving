abe24566f8e11aa27da2f2792d7f913c14a7cc71
sanjay <sanjay.kr.on@gmail.com>
2024-08-09 22:10:40 +0530

    Time: 8 ms (5.38%) | Memory: 44.9 MB (85.87%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,27 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
 
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         List<int[]> resultList = new ArrayList<>( rows * cols + 1 );
         int size = rows * cols ;
         int rEnd = rStart , cEnd = cStart ;
-        int i = rStart , j = cStart , x = 0 , y = 1 , direction = 0 ;
+        int i = rStart , j = cStart , x = 0 , y = 1 ;
         while ( resultList.size() < size ) {
             if ( i < rows && i > -1 && j < cols && j > -1 ) {
                 resultList.add( new int[]{ i , j } );
             }
-            if ( i == rStart && j == cEnd+1 && direction == 0 ) {
-                direction++ ; x++ ; y-- ;
-            } else if ( j == cEnd+1 && i == rEnd+1 && direction == 1 ) {
-                direction++ ; x-- ; y-- ;
-            } else if ( i == rEnd+1 && j == cStart-1 && direction == 2 ) {
-                direction++ ; x-- ; y++ ;
-            } else if ( j == cStart-1 && i == rStart-1 && direction == 3 ) {
-                direction++ ; x++ ; y++ ;
-            } else if ( i == rStart-1 && j == cEnd+1 && direction == 4 ) {
-                direction = 0 ; rStart-- ; cStart-- ; rEnd++ ; cEnd++ ;
+            if ( i == rStart && j == cEnd+1 ) {
+                x++ ; y-- ;
+            } else if ( j == cEnd+1 && i == rEnd+1  ) {
+                x-- ; y-- ;
+            } else if ( i == rEnd+1 && j == cStart-1  ) {
+                x-- ; y++ ;
+            } else if ( j == cStart-1 && i == rStart-1) {
+                x++ ; y++ ;
+            } else if ( i == rStart-1 && j == cEnd+1 ) {
+               rStart-- ; cStart-- ; rEnd++ ; cEnd++ ;
             }
             i += x ; j += y ;
         }
