09c67fcb7e9df8ac07956d3da1074f56ad4eaf81
sanjay <sanjay.kr.on@gmail.com>
2024-08-10 14:54:44 +0530

    Time: 9 ms (97.68%) | Memory: 46.6 MB (26.14%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -9,26 +9,18 @@ class Solution {
         int buff = 0 ;
         for ( int i = 0 ; i < s.length() ; i++ ) {
             char ch = s.charAt(i) ;
-            if (op[ch]==1) {
-                intStack[intIdx++] = buff ;
+            if ( op[ch] == 1) {
+                if ( opIdx > 0 && ( opStack[opIdx-1] == '/' || opStack[opIdx-1] == '*')  ) intStack[intIdx-1] = operate( opStack[--opIdx], intStack[intIdx-1] , buff ) ;
+                else intStack[intIdx++] = buff ;
                 buff = 0 ;
-                if ( opIdx > 0 && ( opStack[opIdx-1] == '/' || opStack[opIdx-1] == '*') ) {
-                    intStack[intIdx - 2] = operate(opStack[--opIdx], intStack[intIdx-2], intStack[intIdx-1]);
-                    intIdx-- ;
-                }
                 opStack[ opIdx++ ] = ch ;
             } else if ( ch != ' ' ) {
                 buff *= 10 ;
                 buff += ch - '0' ;
             }
         }
-        if ( buff > 0 ) {
-            intStack[intIdx++] = buff ;
-            if ( opIdx > 0 && ( opStack[opIdx-1] == '/' || opStack[opIdx-1] == '*') ) {
-                intStack[intIdx - 2] = operate(opStack[--opIdx], intStack[intIdx-2], intStack[intIdx-1]);
-                intIdx-- ;
-            }
-        }
+        if ( opIdx > 0 && ( opStack[opIdx-1] == '/' || opStack[opIdx-1] == '*') ) intStack[intIdx-1] = operate( opStack[--opIdx], intStack[intIdx-1] , buff ) ;
+        else intStack[intIdx++] = buff ;
         int opStartIdx = 0 , intStartIdx = 0 ;
         while ( opStartIdx < opIdx ) {
             intStack[++intStartIdx] = operate( opStack[opStartIdx++] , intStack[intStartIdx-1] , intStack[intStartIdx] ) ;
