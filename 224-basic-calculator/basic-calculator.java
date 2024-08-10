cc9223b88a03ed35150f8b4112d74b9248da7560
sanjay <sanjay.kr.on@gmail.com>
2024-08-11 03:10:00 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (61.99%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,75 +1,27 @@
 class Solution {
+    int i;
     public int calculate(String s) {
-        StringBuilder st = new StringBuilder( s );
-        outer:
-        while ( true ) {
-            int[] stack = new int[ st.length() ] ;
-            int l = 0 , r = st.length() - 1 ;
-            for ( int i = 0 ; i < st.length() ; i++ ) {
-                if ( st.charAt(i) == '(' ) stack[l++] = i ;
-                else if ( st.charAt(i) == ')' ) {
-                    l-- ;
-                    if ( l == 0 ) {
-                        st.replace(
-                                stack[l] , i+1 , calculate(
-                                        st.substring( stack[l]+1 , i )
-                                ) + ""
-                        ) ;
-                        continue outer;
-                    }
-                }
-            }
-            break ;
-        }
-        char [] opStack = new char[st.length()] ;
-        int [] intStack = new int[st.length()] ;
-        int opIdx = 0 , intIdx = 0 , buff = 0 , neg = 1 , c = 0 ;
-        for ( int i = 0 ; i < st.length() ; i++ ) {
-            char ch = st.charAt( i ) ;
-            switch ( ch ) {
-                case ' ' : break;
-                case '+' : case '-' : {
-                    if ( ch == '-' && intIdx == 0 && c == 0 ) {
-                        neg *= -1 ;
-                        continue;
-                    }
-                    if ( c > 0 ) {
-                        intStack[intIdx++] = buff * neg;
-                        neg = 1;
-                        buff = 0;
-                        c = 0;
-                    }
-                    opStack[opIdx++] = ch ;
-                    if ( opIdx > intIdx ) {
-                        if ( opIdx < 2 ) continue;
-                        opIdx-- ;
-                        if ( opStack[opIdx-1] == '+' && ( opStack[opIdx] == '-' || opStack[opIdx] == '+' ) ) opStack[opIdx-1] = opStack[opIdx] ;
-                        else if ( opStack[opIdx-1] == '-' && opStack[opIdx] == '-' ) opStack[opIdx-1] = '+' ;
-                    }
-                } break;
-                default:{
-                    buff *= 10 ;
-                    buff += ch - '0' ;
-                    c++ ;
-                }
+        i = 0 ;
+        return calc(s);
+    }
+    private int calc(String s) {
+        int num = 0, ans = 0;
+        int sign = 1;
+        while (i<s.length()) {
+            char ch = s.charAt(i++);
+            if (ch>='0' && ch<='9') {
+                num = num*10+ch-'0';
+            } else if (ch=='(') {
+                num = calc(s);
+            } else if (ch==')') {
+                return ans+=num*sign;
+            } else if (ch=='+' || ch=='-') {
+                ans+=num*sign;
+                num = 0;
+                sign = (ch=='-')?-1:1;
             }
         }
-        if ( c > 0 ) intStack[intIdx++] = buff * neg ;
-        int opStartIdx = 0 , intStartIdx = 0 ;
-        while ( opStartIdx < opIdx ) {
-            intStack[intStartIdx+1] = operate(
-               opStack[opStartIdx++],
-               intStack[intStartIdx] ,
-               intStack[++intStartIdx]
-            ) ;
-        }
-        return ( intStartIdx < intIdx ) ? intStack[intStartIdx] : 0 ;
+        ans+=num*sign;
+        return ans;
     }
-
-    int operate( char op , int a , int b ) {
-        if ( op == '+' ) return a+b ;
-        else if ( op == '*' ) return a*b ;
-        else return a-b ;
-    }
-
 }
