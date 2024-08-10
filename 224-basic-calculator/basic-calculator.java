9118017c16b833c1406ec5457317a0e5fb4bcc10
sanjay <sanjay.kr.on@gmail.com>
2024-08-11 04:20:49 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (53.73%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,27 +1,27 @@
 class Solution {
-    int i;
+
+    int index;
+
     public int calculate(String s) {
-        i = 0 ;
-        return calc(s);
+        index = 0 ;
+        return evaluate(s);
     }
-    private int calc(String s) {
+
+    int evaluate(String s) {
         int num = 0, ans = 0;
         int sign = 1;
-        while (i<s.length()) {
-            char ch = s.charAt(i++);
-            if (ch>='0' && ch<='9') {
-                num = num*10+ch-'0';
-            } else if (ch=='(') {
-                num = calc(s);
-            } else if (ch==')') {
-                return ans+=num*sign;
-            } else if (ch=='+' || ch=='-') {
-                ans+=num*sign;
-                num = 0;
-                sign = (ch=='-')?-1:1;
+        while ( index < s.length() ) {
+            char ch = s.charAt(index++);
+            if ( ch >='0' && ch <= '9' ) num = num * 10 + ch - '0' ;
+            else if ( ch == '(' ) num = evaluate(s);
+            else if ( ch == ')' ) return ans+num*sign;
+            else if ( ch == '+' || ch == '-' ) {
+                ans += num * sign ;
+                num = 0 ;
+                // ascii value '+' = 43, '-'  = 45
+                sign = (ch-44)*-1;
             }
         }
-        ans+=num*sign;
-        return ans;
+        return ans + num * sign ;
     }
 }
