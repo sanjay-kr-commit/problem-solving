bfd5dabb03182e72be7e6bba1559b08145c330fe
sanjay <sanjay.kr.on@gmail.com>
2024-08-11 05:37:09 +0530

    Time: 2 ms (99.70%) | Memory: 49.3 MB (59.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,27 +1,25 @@
 class Solution {
 
-    int island ;
-
     public int numIslands(char[][] grid) {
-        island = 0;
+        int island = 0;
+        int m = grid.length, n = grid[0].length;
         for ( int i = 0 ; i < grid.length ; i++ ) {
             for ( int j = 0 ; j < grid[0].length ; j++ ) {
                 if ( grid[i][j] == '1' ) {
                     island++ ;
-                    annihilateIsland( grid , i , j );
+                    annihilateIsland( grid , i , j , m , n );
                 }
             }
         }
         return island ;
     }
 
-    void annihilateIsland( char[][] grid , int i , int j ) {
-        if ( i < 0 || i >= grid.length || j >= grid[i].length || j < 0 || grid[i][j] == '0'  ) return;
-        grid[i][j] = '0' ;
-        annihilateIsland( grid , i+1 , j );
-        annihilateIsland( grid , i , j-1 );
-        annihilateIsland( grid , i , j+1 );
-        annihilateIsland( grid , i-1 , j );
+    void annihilateIsland( char[][] grid , int i , int j , int m , int n ) {
+        grid[i][j] = '2' ;
+        if ( i+1 < m && grid[i+1][j] == '1' ) annihilateIsland( grid , i+1 , j , m , n );
+        if ( j > 0 && grid[i][j-1] == '1' ) annihilateIsland( grid , i , j-1 , m , n );
+        if ( j+1 < n && grid[i][j+1] == '1' ) annihilateIsland( grid , i , j+1 , m , n );
+        if ( i > 0 && grid[i-1][j] == '1' ) annihilateIsland( grid , i-1 , j , m , n );
     }
 
 }
