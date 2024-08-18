1191122e74016ef9c49c142d0dcff95846fca921
sanjay <sanjay.kr.on@gmail.com>
2024-08-18 13:18:26 +0530

    Time: 1 ms (92.91%) | Memory: 41.2 MB (78.55%) - LeetSync

--- 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.java
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.java
@@ -1,23 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
     public int minimumOperationsToMakeEqual(int x, int y) {
-        if (x <= y) return y - x;
-        if (map.containsKey(x)) return map.get(x);
-        int res = Integer.MAX_VALUE;
-        //try decrease to a number dividable by 5;
-        res = Math.min(minimumOperationsToMakeEqual(x/5, y) + x % 5 + 1, res);
-        //try increase to a number dividable by 5;
-        res = Math.min(minimumOperationsToMakeEqual(x/5 + 1, y) + (5 - x % 5) + 1, res);
-        //try decrease to a number dividable by 11;
-        res = Math.min(minimumOperationsToMakeEqual(x/11, y) + x % 11 + 1, res);
-        //try increase to a number dividable by 11;
-        res = Math.min(minimumOperationsToMakeEqual(x/11 + 1, y) + (11 - x % 11) + 1, res);
-        //try decrease to y directly.
-        res = Math.min(x - y, res);
-        map.put(x, res);
+        if (x <= y)
+            return y - x;
+        int res = x - y;
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 5, y) + 1 + x % 5);
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 5 + 1, y) + 1 + 5 - x % 5);
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 11, y) + 1 + x % 11);
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 11 + 1, y) + 1 + 11 - x % 11);
         return res;
     }
 }
