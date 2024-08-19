507923b64a46e5908eb942377ff3cb514dfd7925
sanjay <sanjay.kr.on@gmail.com>
2024-08-19 22:41:10 +0530

    Time: 10 ms (99.65%) | Memory: 57.7 MB (14.49%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
@@ -1,19 +1,43 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public int maximumProduct(int[] nums, int k) {
-        long mod=(long) (1e9)+7;
-        if ( nums.length == 1 ) return (int) ((nums[0]+k)%mod) ;
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        for( int num : nums ) pq.add( (long) num );
-        while( k > 0 ){
-            long top = pq.remove() ;
-            long add = (pq.isEmpty()) ? k : Math.min( k , (pq.peek() - top) + 1 ) ;
-            pq.add( top+add ) ;
-            k -= (int) add ;
+        int left = 0;
+        int right = 2000001;
+        int mid = left+(right-left+1)/2;
+        while(right>left){
+            if(extraK(nums,k,mid)>=0){//valid guess
+                left=mid;
+            }else{
+                right=mid-1;
+            }
+            mid = left+(right-left+1)/2;
         }
-        long product=1;
-        while(!pq.isEmpty()) product = ( product * pq.remove() ) %mod ;
-        return (int)product;
+        int minVal=left;
+        int extra = extraK(nums,k,mid);
+        long ans = 1;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]<=minVal){
+                if(extra>0){
+                    extra--;
+                    ans*=minVal+1;
+                }else{
+                    ans*=minVal;
+                }
+            }else{
+                ans*=nums[i];
+            }
+            ans%=1000000007L;
+        }
+        return (int)ans;
+    }
+    public int extraK(int[]nums,int k,int guess){
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]<guess){
+                k-=guess-nums[i];
+            }
+            if(k<0){
+                return -1;
+            }
+        }
+        return k;
     }
 }
