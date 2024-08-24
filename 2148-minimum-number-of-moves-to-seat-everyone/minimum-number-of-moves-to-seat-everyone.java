dbe08207046da101ec15311cfcd6d7e405c61878
sanjay <sanjay.kr.on@gmail.com>
2024-08-24 18:39:28 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (11.63%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -1,11 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        int move = 0 ;
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        for ( int i = 0 ; i < seats.length ; i++ ) move += Math.abs( seats[i] - students[i] );
-        return move ;
+        int[] seatOccupied = new int[101];
+        int[] studentOccupied = new int[101];
+        for ( int i = 0 ; i < seats.length ; i++ ) {
+            seatOccupied[seats[i]]++;
+            studentOccupied[students[i]]++;
+        }
+        int move = 0;
+        for ( int i = 0 , j = 0 , k = 0 ; i < students.length ; i++ ) {
+            while ( j < 101 && seatOccupied[j] == 0 ) j++ ;
+            while ( k < 101 && studentOccupied[k] == 0 ) k++ ;
+            if ( j == 101 || k == 101 ) break;
+            int diff = Math.min(seatOccupied[j] , studentOccupied[k]);
+            move += diff * Math.abs( j - k ) ;
+            seatOccupied[j]-=diff;
+            studentOccupied[k]-=diff;
+        }
+        return move;
     }
 }
