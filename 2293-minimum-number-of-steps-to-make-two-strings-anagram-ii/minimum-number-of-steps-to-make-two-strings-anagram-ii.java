5c19f41f571e199af09dc83726b2b5b197aee6af
sanjay <sanjay.kr.on@gmail.com>
2024-10-08 19:50:38 +0530

    Time: 12 ms (66.33%) | Memory: 46.6 MB (73.00%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -4,10 +4,7 @@ class Solution {
         int [] freq = new int[26];
         for ( int i = 0 ; i < s.length(); i++ ) freq[s.charAt(i) - 'a']++;
         for ( int i = 0 ; i < t.length() ; i++ ) freq[t.charAt(i) - 'a']--;
-        for ( int i = 0 ; i < 26 ; i++ ) {
-            if ( freq[i] == 0 ) continue; 
-            appendCount += Math.abs( freq[i] );
-        }
+        for ( int i = 0 ; i < 26 ; i++ ) appendCount += Math.abs( freq[i] );
         return appendCount ;
     }
 }
