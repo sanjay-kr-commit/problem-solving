0e1f495a4c6aa8ab1adaf43ba1c599aaeb73632f
sanjay <sanjay.kr.on@gmail.com>
2024-10-12 00:06:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (50.32%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,14 +1,10 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        char [] buff = new char[ word1.length() + word2.length() ];
-        int i = 0 , j = 0 , k = 0 , len1 = word1.length() , len2 = word2.length() ;
-        boolean turn = true ;
-        while ( i < len1 && j < len2 ) {
-            buff[k++] = ( turn ) ? word1.charAt(i++) : word2.charAt(j++) ;
-            turn = !turn ;
-        }
-        while ( i < len1 ) buff[k++] = word1.charAt(i++) ;
-        while ( j < len2 ) buff[k++] = word2.charAt(j++) ;
-        return new String( buff ) ;
+        int i = 0 , j = 0 , k = 0 , m = word1.length() , n = word2.length() ;
+        char [] mergedString = new char [ m+n ] ;
+        while ( j < m && k < n ) mergedString[i++] = ( i % 2 != 0 ) ? word1.charAt( j++ ) : word2.charAt( k++ ) ;
+        while ( j < m ) mergedString[i++] = word1.charAt( j++ ) ;
+        while ( k < n ) mergedString[i++] = word2.charAt( k++ ) ;
+        return new String( mergedString ) ;
     }
 }
