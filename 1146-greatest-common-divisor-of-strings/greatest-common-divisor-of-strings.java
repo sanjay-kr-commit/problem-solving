d0211a5b2ae1a295d1f5b65702591db36eae1bca
sanjay <sanjay.kr.on@gmail.com>
2024-10-12 01:26:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (77.95%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,26 +1,8 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        if ( str1.length() < str2.length() ) return gcdOfStrings(str2, str1);
-        int i , j , m = str1.length() , n = str2.length() ;
-        for ( i = 0 , j = 0 ; i < m ; i++ , j++ ) {
-            if ( str1.charAt(i) != str2.charAt(j%n) ) break;
-        }
-        if ( i != m ) return "" ;
-        n = gcd( m , n ) ;
-        while ( n > 0 ) if ( m%n != 0 ) n-- ;
-        else {
-            for ( i = 0 , j = 0 ; i < m ; i++ , j++ ) {
-                if ( str1.charAt(i) != str2.charAt(j%n) ) break ;
-            }
-            if ( i == m && m % n == 0 ) break ;
-            else n-- ;
-        }
-        return str2.substring( 0 , n ) ;
-    }
-
-    public int gcd(int a, int b) {
-        if (b==0) return a;
-        return gcd(b,a%b);
-    }
-
+    public static String gcdOfStrings(String str1, String str2) {
+	        if(str1.equals(str2)) return str1;
+	        if(str1.length()<str2.length()) return gcdOfStrings(str2, str1);
+	        if(str1.startsWith(str2) && !str1.equals(str2)) return gcdOfStrings(str1.substring(str2.length()), str2);
+	       return "";
+	    }
 }
