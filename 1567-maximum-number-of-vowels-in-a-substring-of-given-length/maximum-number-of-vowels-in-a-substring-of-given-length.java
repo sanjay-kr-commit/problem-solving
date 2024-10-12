e5e201e13afe0cba2869b7abd6bcd51ee06ae20b
sanjay <sanjay.kr.on@gmail.com>
2024-10-12 17:22:22 +0530

    Time: 7 ms (98.22%) | Memory: 45.1 MB (17.14%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -34,7 +34,4 @@ class Solution {
         return maxVowelCount ;
     }
 
-//    boolean isVowel( char c ) {
-//        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ;
-//    }
 }
