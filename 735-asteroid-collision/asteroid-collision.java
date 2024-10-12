654cdb0d03e80b3a8dacc4621fef40097a5b632b
sanjay <sanjay.kr.on@gmail.com>
2024-10-13 00:09:22 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (73.30%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,20 +1,19 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int [] collisionStats = new int[ asteroids.length ] ;
-        int size = -1 ;
-        for ( int asteroid : asteroids ) {
-            boolean isNegative = asteroid < 0 ;
-            int val = asteroid * ((isNegative)?-1:1) ;
-            if ( !isNegative || size == -1 || collisionStats[size] < 0 ) collisionStats[++size] = asteroid ;
-            else {
-                while ( size != -1 && collisionStats[size] > 0 && collisionStats[size] < val ) size-- ;
-                if ( size != -1 && collisionStats[size] == val ) size-- ;
-                else if ( size == -1 || collisionStats[size] < 0 || collisionStats[size] < val ) collisionStats[++size] = asteroid ;
+        int [] stack = new int[ asteroids.length ] ;
+        int stackSize = 0 ;
+        for ( int i = 0 ; i < asteroids.length ; i++ ) {
+            stack[stackSize++] = asteroids[i] ;
+            while ( stackSize > 1 && stack[stackSize-1] < 0 && stack[stackSize-2] > -1 ) {
+                int a = Math.abs(stack[stackSize-2]) ,
+                    b = Math.abs(stack[stackSize-1]) ;
+                stackSize-- ;
+                if ( a == b ) stackSize-- ;
+                else if ( b > a ) stack[stackSize-1] = stack[stackSize] ;
             }
         }
-        size++ ;
-        int [] collisionImpact = new int[ size ] ;
-        System.arraycopy(collisionStats, 0, collisionImpact, 0, size);
-        return collisionImpact ;
+        return Arrays.copyOf( stack , stackSize ) ;
     }
 }
