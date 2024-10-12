f60ba0a64226057254d428a45ac7e0c7e61337c0
sanjay <sanjay.kr.on@gmail.com>
2024-10-13 02:14:19 +0530

    Time: 19 ms (99.95%) | Memory: 78.7 MB (6.31%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -2,24 +2,18 @@ import java.util.Arrays;
 
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        int arrowThrown = 0 ;
-        Arrays.sort( points , (o1, o2) -> {
-            if ( o1[0] == o2[0] ) {
-                return Integer.compare(o1[1], o2[1]);
-            } else if ( o1[0] < o2[0] ) return -1 ;
-            else return 1 ;
-        });
-        int start = -1 , end = -1 ;
-        for ( int [] point : points ) {
-            if ( point[0] >= start && point[0] <= end ) {
-                start = point[0] ;
-                if ( point[1] < end ) end = point[1] ;
-            } else {
-                arrowThrown++ ;
-                start = point[0] ;
-                end = point[1] ;
-            }
+        final int n = points.length;
+        final long[] merged = new long[n];
+        for (int i = 0; i < n; i++) merged[i] = (((long)points[i][1]) << 32) | (points[i][0] & 0xFFFFFFFFL);
+        Arrays.sort(merged);
+        int prev = ( int )( merged[0] >>> 32 );
+        int count = 1;
+        for( int i = 1 ; i < merged.length ; i++ ) if ( (int) merged[i] > prev ) {
+            count++;
+            prev = (int)(merged[i] >>> 32);
         }
-        return arrowThrown;
+
+        return count;
+
     }
 }
