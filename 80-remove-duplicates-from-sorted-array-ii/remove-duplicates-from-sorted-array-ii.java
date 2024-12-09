10cebc9798a0cc5ecd062d61a56e63ddcf074b5e
sanjay <sanjay.kr.on@gmail.com>
2024-12-09 23:24:13 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (8.03%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int slowPointer = 0,
-            fastPointer = 0 ,
-            size = nums.length;
-        while ( fastPointer < size ) {
-            // initial shift
-            nums[slowPointer] = nums[fastPointer++] ;
-            if ( fastPointer < size && nums[slowPointer] == nums[fastPointer] ) 
-                nums[++slowPointer] = nums[fastPointer++] ;
-            while ( fastPointer < size && nums[slowPointer] == nums[fastPointer] ) fastPointer++ ;
-            slowPointer++ ;
-        }
-        return slowPointer ;
+      int fastPointer = 0 ,
+          slowPointer = 0 ,
+          size = nums.length ;
+      while ( fastPointer < size ) {
+        nums[slowPointer] = nums[fastPointer++] ;
+        if ( fastPointer < size && nums[slowPointer] == nums[fastPointer] ) {
+          nums[++slowPointer] = nums[fastPointer++] ;
+        } 
+        while ( fastPointer < size && nums[fastPointer] == nums[slowPointer] ) fastPointer++ ;
+        slowPointer++ ;
+      }
+      return slowPointer ;
     }
 }
