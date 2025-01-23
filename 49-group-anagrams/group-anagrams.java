1c06969b907b5f785ec8ac499df817414e448722
sanjay <sanjay.kr.on@gmail.com>
2025-01-23 15:50:45 +0530

    Time: 9 ms (35.35%) | Memory: 47.6 MB (94.09%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,34 +1,23 @@
 import java.util.*;
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        ArrayList <List<String>> rest = new ArrayList<>() ;
-        HashSet <Integer> used = new HashSet<>() ;
-        for ( int i = 0 , len = strs.length ; i < len ; i++ ) {
-            String str = strs[i] ;
-            if ( used.contains(i) ) continue ;
-            else used.add(i) ;
-            ArrayList <String> subList = new ArrayList<>() ;
-            subList.add( str ) ;
-            int [] freqStr = new int [ 26 ] ;
-            for ( int j = 0 , jlen = str.length() ; j < jlen ; j++ ) {
-                freqStr[str.charAt(j)-'a']++ ;
-            }
-            for ( int j = 0 ; j < len ; j++ ) {
-                if ( used.contains(j) ) continue ;
-                int [] subFreqStr = new int [26] ;
-                for ( int k = 0 , klen = strs[j].length() ; k < klen ; k++ ) {
-                    subFreqStr[ strs[j].charAt(k)-'a' ]++ ;
-                }
-                int k = 0 ;
-                for (; k < 26 ; k++ ) {
-                    if ( subFreqStr[k] != freqStr[k] ) break ;
-                }
-                if ( k < 26 ) continue ;
-                subList.add( strs[j] ) ;
-                used.add(j) ;
-            }
-            rest.add(subList) ;
+        ArrayList <List<String>> result = new ArrayList<>();
+        
+        HashMap<String , ArrayList<String>> map = new HashMap<>();
+        
+        for ( String str : strs ) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            
+            if ( map.containsKey(sorted) ) map.get(sorted).add(str);
+            else map.put(sorted, new ArrayList<>( List.of(str) ));
         }
-        return rest ;
+        
+        for ( Map.Entry<String, ArrayList<String>> entry : map.entrySet() ) {
+            result.add(new ArrayList<>(entry.getValue()));
+        }
+        
+        return result ;
     }
 }
