5eb32d4b1d775f6f51cba4c228d72d03b5be3961
sanjay <sanjay.kr.on@gmail.com>
2025-01-27 08:31:05 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (90.30%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,19 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-  
-  public List<Integer> rightSideView(TreeNode root) {
-    ArrayList<Integer> res = new ArrayList<>() ;
-    addRightView( root , new HashSet<Integer>() , res , 0 ) ;
-    return res ;
-  }
-
-  void addRightView( TreeNode node , HashSet<Integer> levelOccupied , ArrayList<Integer> res , int level ) {
-    if ( node == null ) return ;
-    if ( !levelOccupied.contains( level ) ) {
-      levelOccupied.add( level ) ;
-      res.add( node.val ) ;
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traverseTree(root, 0, list);
+        return list;
     }
-    addRightView( node.right , levelOccupied , res , level+1 ) ;
-    addRightView( node.left , levelOccupied , res , level+1 ) ;
-  }
 
+    public void traverseTree(TreeNode node, int level, List<Integer> list){
+        if(node == null) return;
+        if(level == list.size()) list.add(node.val); 
+        traverseTree(node.right, level+1, list);
+        traverseTree(node.left, level+1, list);
+    }
 }
