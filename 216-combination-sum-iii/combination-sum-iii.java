e0b6181cc678a09ae58e0d340f869b2f6254259c
sanjay <sanjay.kr.on@gmail.com>
2025-01-31 23:45:18 +0530

    Time: 1 ms (20.90%) | Memory: 41.4 MB (18.51%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -5,34 +5,38 @@ import java.util.List;
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         ArrayList<List<Integer>> combinations = new ArrayList<>();
-        calculatePermutation(
-                k , n , combinations
-                , 0L , new HashSet<>() ,
-                0 , 0
+        calculatePermutations(
+                k , n , 0 , 0 , 0 , combinations , new HashSet<>()
         );
         return combinations ;
     }
 
-    void calculatePermutation(
-            int places , int target , ArrayList<List<Integer>> combinations ,
-            long occupiedNumber , HashSet<Long> occupiedSet , int sum , int placesOccupied
+    void calculatePermutations(
+            int places , int target , int sum , int placesOccupied , int numOccupied ,
+            ArrayList<List<Integer>> combinations , HashSet<Integer> occupiedSet
     ) {
-        if ( placesOccupied == places ) {
-            if ( sum != target || occupiedSet.contains( occupiedNumber ) ) return;
-            occupiedSet.add( occupiedNumber );
-            ArrayList<Integer> combination = new ArrayList<>();
-            for ( int i = 0 ; i < 60 ; i++ ) {
-                if ( ((occupiedNumber>>i)&1L) != 1L ) continue;
-                combination.add(i+1);
+        if ( occupiedSet.contains( numOccupied ) ) return;
+        occupiedSet.add( numOccupied );
+        if ( places == placesOccupied ) {
+            if ( sum != target ) return;
+            ArrayList<Integer> combination = new ArrayList<>( places );
+            for ( int i = 0 ; i < 9 ; i++ ) {
+                if ( (numOccupied&1) == 1 ) combination.add( i + 1 ) ;
+                numOccupied >>= 1 ;
             }
-            combinations.add(combination);
+            combinations.add( combination );
+            return;
         }
-        if ( occupiedSet.contains( occupiedNumber) || sum >= target ) return;
-        occupiedSet.add( occupiedNumber );
+        if ( sum >= target ) return;
+        long localNumOccupied = numOccupied ;
         for ( int i = 0 ; i < 9 ; i++ ) {
-            if ( ((occupiedNumber>>i)&1L) == 1L ) continue;
-            occupiedSet.add( occupiedNumber );
-            calculatePermutation( places , target , combinations , (occupiedNumber | ( 1L << i )) , occupiedSet , sum+i+1 , placesOccupied+1 ) ;
+            if ( (localNumOccupied & 1) == 1 ) continue;
+            calculatePermutations(
+                    places , target , sum + i + 1 , placesOccupied+1 , ( numOccupied | ( 1 << i ) ) ,
+                    combinations , occupiedSet
+            );
+            localNumOccupied >>= 1 ;
         }
     }
+
 }
