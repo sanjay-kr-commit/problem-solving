51189b1ffb203cedcc5f1ee7c2b00bfad7c4e37e
sanjay <sanjay.kr.on@gmail.com>
2025-02-15 04:12:17 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (66.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,19 +1,19 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9] ,
-                    cols = new boolean[9][9] ,
-                    grid = new boolean[9][9] ;
-        for ( int i = 0; i < 9; i++ ) {
-            for ( int j = 0; j < 9; j++ ) {
-                if ( board[i][j] == '.' ) continue;
-                int value = board[i][j] - '1';
-                int gridIndex = (i/3) * 3 + (j/3) ;
-                if ( rows[i][value] || cols[j][value] || grid[gridIndex][value] ) return false ;
-                rows[i][value] = true;
-                cols[j][value] = true;
-                grid[gridIndex][value] = true;
-            }
-        }
-        return true ;
+  public boolean isValidSudoku(char[][] board) {
+    boolean [][] rows = new boolean [9][9] ,
+                 cols = new boolean [9][9] ,
+                 grid = new boolean [9][9] ;
+    for ( int i = 0 ; i < 9 ; i++ ) {
+      for ( int j = 0 ; j < 9 ; j++ ) {
+        if ( board[i][j] == '.' ) continue ;
+        int value = board[i][j] - '1' ;
+        int gridIndex = ( i / 3 ) * 3 + ( j / 3 ) ;
+        if ( rows[i][value] || cols[j][value] || grid[gridIndex][value] ) return false ;
+        rows[i][value] = true ;
+        cols[j][value] = true ;
+        grid[gridIndex][value] = true ;
+      }
     }
+    return true ;
+  }
 }
