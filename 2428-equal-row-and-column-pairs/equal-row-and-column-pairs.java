72a5b79f5dc949f35b2c962d2aa3f6500d534a3a
sanjay <sanjay.kr.on@gmail.com>
2025-02-16 00:40:40 +0530

    Time: 35 ms (47.07%) | Memory: 52.2 MB (32.31%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -3,25 +3,21 @@ class Solution {
     public int equalPairs(int[][] grid) {
         HashMap<String,Integer> set = new HashMap<>();
         int count = 0 ;
-        StringBuilder[] cols = new StringBuilder[ grid.length ] ;
-        for ( int i = 0 ; i < grid.length ; i++ ) {
-            cols[i] = new StringBuilder() ;
-        }
-        for ( int i = 0 ; i < grid.length ; i++ ) {
-            StringBuilder row = new StringBuilder() ;
-            for ( int j = 0 ; j < grid.length ; j++ ) {
-                row.append( grid[i][j] ) ;
-                row.append( "," ) ;
-                cols[j].append( grid[i][j] ) ;
-                cols[j].append( "," ) ;
+        for ( int[] row : grid ) {
+            StringBuilder builder = new StringBuilder();
+            for ( int col : row ) {
+                builder.append( col );
+                builder.append( "," );
             }
-            if ( set.containsKey( row.toString() ) ) set.put( row.toString() , set.get( row.toString() ) + 1 ) ;
-            else set.put( row.toString() , 1 ) ;
+            set.put( builder.toString(), set.getOrDefault( builder.toString(), 0) + 1 );
         }
         for ( int i = 0 ; i < grid.length ; i++ ) {
-            Integer val = set.get( cols[i].toString() ) ;
-            if ( val == null ) val = 0 ;
-            count += val ;
+            StringBuilder builder = new StringBuilder();
+            for ( int j = 0 ; j < grid.length ; j++ ) {
+                builder.append( grid[j][i] ) ;
+                builder.append( "," );
+            }
+            count += set.getOrDefault( builder.toString(), 0 );
         }
         return count ;
     }
