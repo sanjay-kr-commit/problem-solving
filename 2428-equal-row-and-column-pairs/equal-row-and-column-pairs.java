37ad28ba034eb3371f697afe04d9dbbf5dfd1d4b
sanjay <sanjay.kr.on@gmail.com>
2025-02-16 00:45:11 +0530

    Time: 2 ms (100.00%) | Memory: 47.8 MB (88.05%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,25 +1,29 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
+    private int hashRow(int[] row){
+        int hash=0;
+        for (int n : row){
+            hash = n+hash*5;
+        }
+        return hash;
+    }
+    private int hashCol(int[][] grid, int col){
+        int hash=0;
+        for (int[] ints : grid) {
+            hash = ints[col] + hash * 5;
+        }
+        return hash;
+    }
     public int equalPairs(int[][] grid) {
-        HashMap<String,Integer> set = new HashMap<>();
-        int count = 0 ;
-        for ( int[] row : grid ) {
-            StringBuilder builder = new StringBuilder();
-            for ( int col : row ) {
-                builder.append( col );
-                builder.append( "," );
-            }
-            set.put( builder.toString(), set.getOrDefault( builder.toString(), 0) + 1 );
+        int n = grid.length , count = 0;
+        Map<Integer, Integer> rows = new HashMap<>();
+        for ( int [] row : grid ) {
+            int hashRow = hashRow(row);
+            rows.put(hashRow, rows.getOrDefault(hashRow,0)+1);
         }
-        for ( int i = 0 ; i < grid.length ; i++ ) {
-            StringBuilder builder = new StringBuilder();
-            for ( int j = 0 ; j < grid.length ; j++ ) {
-                builder.append( grid[j][i] ) ;
-                builder.append( "," );
-            }
-            count += set.getOrDefault( builder.toString(), 0 );
-        }
-        return count ;
+        for ( int i = 0 ; i < n ; i++ ) count += rows.getOrDefault(hashCol(grid,i),0);
+        return count;
     }
 }
-
