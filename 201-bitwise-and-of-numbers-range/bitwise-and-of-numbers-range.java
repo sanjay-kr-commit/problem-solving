ec6d22d9ad4f59fe2a0d5abc73514b6b4b4b527f
sanjay <sanjay.kr.on@gmail.com>
2025-02-23 16:04:00 +0530

    Time: 4 ms (10.60%) | Memory: 43.9 MB (59.35%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -3,11 +3,9 @@ class Solution {
         int diff = right - left ;
         int newNum = 0 , power = 1 ;
         for ( int i = 0 ; i < 32 ; i++ ) {
-            if ( (right & 1) == 1 && (left & 1) == 1 && diff < power ) {
+            if ( diff < power && ((right >> i) & 1) == 1 && ((left>>i) & 1) == 1 ) {
                 newNum = (newNum) | (1 << i) ;
             }
-            right = right >> 1 ;
-            left = left >> 1 ;
             power <<= 1 ;
         }
         return newNum ;
