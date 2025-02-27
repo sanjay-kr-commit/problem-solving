43c9384d89e79d04182e446eff2a87c7a577f407
sanjay <sanjay.kr.on@gmail.com>
2025-02-27 20:25:57 +0530

    Time: 8 ms (99.78%) | Memory: 45.9 MB (36.24%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,35 +1,41 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if ( word1.length() != word2.length() ) return false;
-        int [] map1 = new int[26] ,
-               map2 = new int[26];
-        int min = 0 , max = 0 ;
-        for ( int i = 0 ; i < word1.length() ; i++ ) {
-            int j = ++map1[word1.charAt(i) - 'a'];
-            int k = ++map2[word2.charAt(i) - 'a'];
-            min = Math.min(min, Math.min( j , k ));
-            max = Math.max(max, Math.max( j , k ));
-        }
-        for ( int i = 0 ; i < 26 ; i++ ) {
-            if ( map1[i] == 0 ^ map2[i] == 0 ) return false;
-        }
-        int [] freq1 = new int[max - min + 1] ,
-               freq2 = new int[max - min + 1];
-        for ( int i : map1 ) freq1[i-min]++ ;
-        for ( int i : map2 ) freq2[i-min]++ ;
-        for ( int i = 0 ; i< freq1.length ; i++ ) {
-            if ( freq1[i] != freq2[i] ) return false;
-        }
-        return true;
-    }
+        if (word1.length() != word2.length()) return false;
+        if (word1.equals(word2)) return true;
 
-    boolean containsSameCharacters(String word1, String word2) {
-        int word1Mask = 0 , word2Mask = 0 ;
-        for ( int i = 0 ; i < word1.length() ; i++ ) {
-            word1Mask |= 1 << word1.charAt(i)-'a' ;
-            word2Mask |= 1 << word2.charAt(i)-'a' ;
-        }
-        return word1Mask == word2Mask;
-    }
+        int[] freq1 = new int['z' + 1];
+        int[] freq2 = new int['z' + 1];
 
+        for ( char c : word1.toCharArray() ) freq1[c]++;
+        for ( char c : word2.toCharArray() ) freq2[c]++;
+
+        for (int i = 'a'; i <= 'z'; i++)
+            if (freq1[i] == 0 ^ freq2[i] == 0)
+                return false;
+
+        // sort count
+        freq1['a' - 1] = -1;
+        for ( int i , k = 'a' ; k <= 'z'; k++ ) {
+            int f1i = freq1[i = k];
+            while (f1i < freq1[--i])
+                freq1[i + 1] = freq1[i];
+            freq1[i + 1] = f1i;
+        }
+
+        // sort count
+        freq2['a' - 1] = -1;
+        for ( int i , k = 'a' ; k <= 'z'; k++ ) {
+            int f2i = freq2[i = k];
+            while (f2i < freq2[--i])
+                freq2[i + 1] = freq2[i];
+            freq2[i + 1] = f2i;
+        }
+
+        boolean result = true;
+
+        for (int i = 'a'; result && i <= 'z'; i++)
+            result = freq1[i] == freq2[i] ;
+
+        return result;
+    }
 }
