5d1699368bca8137231fb9695dec182c241fc59e
sanjay <sanjay.kr.on@gmail.com>
2025-02-27 19:54:25 +0530

    Time: 23 ms (50.23%) | Memory: 46 MB (24.01%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if ( word1.length() != word2.length() || !containsSameCharacters( word1, word2 ) ) return false;
+        int [] map1 = new int[26] ,
+               map2 = new int[26];
+        int min = 0 , max = 0 ;
+        for ( int i = 0 ; i < word1.length() ; i++ ) {
+            int j = ++map1[word1.charAt(i) - 'a'];
+            int k = ++map2[word2.charAt(i) - 'a'];
+            min = Math.min(min, Math.min( j , k ));
+            max = Math.max(max, Math.max( j , k ));
+        }
+        int [] freq1 = new int[max - min + 1] ,
+               freq2 = new int[max - min + 1];
+        for ( int i : map1 ) freq1[i-min]++ ;
+        for ( int i : map2 ) freq2[i-min]++ ;
+        for ( int i = 0 ; i< freq1.length ; i++ ) {
+            if ( freq1[i] != freq2[i] ) return false;
+        }
+        return true;
+    }
+
+    boolean containsSameCharacters(String word1, String word2) {
+        int word1Mask = 0 , word2Mask = 0 ;
+        for ( int i = 0 ; i < word1.length() ; i++ ) {
+            word1Mask |= 1 << word1.charAt(i)-'a' ;
+            word2Mask |= 1 << word2.charAt(i)-'a' ;
+        }
+        return word1Mask == word2Mask;
+    }
+
+}
