a84358462eaf54a2260e216f480e83ed86a22bc7
sanjay <sanjay.kr.on@gmail.com>
2025-03-04 20:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (78.49%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        int used = 0 ;
         int count = 0 ;
+        int used = 0 ;
         while ( count < n ) {
-            int required = n - count;
-            int localCount = 1 , prev = 1 ;
+            int required = n - count ;
+            int prev = 1 ;
+            int localCount = 1 ;
             int power = 0 ;
             while ( localCount < required && power < 17 ) {
                 prev = localCount ;
@@ -16,21 +17,13 @@ class Solution {
                 used |= 1 << power ;
                 continue;
             }
-
-            if ( power > 0 && prev <= required && ((used >> power-1) & 1 ) == 0 ) {
+            power-- ;
+            if ( power > 0 && ((used >> power) & 1 ) == 0 ) {
                 count += prev ;
-                used |= 1 << (power-1) ;
+                used |= 1 << power ;
                 continue;
             }
-
-            if ( power < 17 && ((used >> power+1) & 1 ) == 0 ) {
-                count += localCount*3 ;
-                used |= 1 << (power+1) ;
-                continue;
-            }
-
             break;
-
         }
         return count == n ;
     }
