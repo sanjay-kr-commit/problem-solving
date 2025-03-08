57a5ad12149f16c61f58b849c59e36b060496067
sanjay <sanjay.kr.on@gmail.com>
2025-03-08 12:29:57 +0530

    Time: 3 ms (26.32%) | Memory: 42.2 MB (97.96%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String clearDigits(String s) {
+        boolean [] occupied = new boolean[ s.length() ];
+        for ( int j , i = 0 ; i < s.length() ; i++ ) {
+            if ( s.charAt( i ) >= 'a' && s.charAt( i ) <= 'z' ) {
+                continue ;
+            }
+            for ( j = i-1 ; j > -1 ; j-- ) {
+                if ( occupied[j] ) continue;
+                if ( s.charAt( j ) >= '0' && s.charAt( j ) <= '9' ) continue;
+                break ;
+            }
+            if ( j > -1 ) {
+                occupied[ j ] = true;
+                occupied[ i ] = true;
+            }
+        }
+        char [] str = new char[s.length()];
+        int len = 0 ;
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            if ( occupied[i] ) continue;
+            str[len++] = s.charAt(i);
+        }
+        return new String(str, 0, len);
+    }
+}
