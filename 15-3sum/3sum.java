4bd1d6fbb1a4c9c9545ecc6706682f5c0753f7c1
sanjay <sanjay.kr.on@gmail.com>
2025-03-19 02:36:18 +0530

    Time: 1164 ms (6.78%) | Memory: 53.3 MB (9.50%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,35 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<String> set = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        ArrayList<List<Integer>> result = new ArrayList<>( nums.length );
-        for ( int i = 0 , len = nums.length ; i < len ; i++ ) {
-            int left = i + 1 , right = len - 1;
+        outer:
+        for ( int i = 0 ; i < nums.length-2; i++ ) {
             int x = -nums[i] ;
+            int left = i+1 , right = nums.length - 1 ;
             while ( left < right ) {
-                int sum = nums[left] + nums[right];
-                if ( sum > x ) right-- ;
-                else if ( sum < x ) left++ ;
-                else {
-                    ArrayList<Integer> toAdd = new ArrayList<>( 3 );
-                    toAdd.add( nums[i] );
-                    toAdd.add( nums[left] );
-                    toAdd.add( nums[right] );
-                    result.add(toAdd);
-                    len-- ;
-                    while ( left < len && nums[left] == nums[left + 1] ) left++ ;
-                    len++ ;
-                    while ( right > left && nums[right] == nums[right-1] ) right-- ;
-                    left++ ;
-                    right-- ;
+                int y = x-nums[left] ;
+                if ( y < nums[left]) continue outer;
+                while ( right > left && nums[right] > y ) right-- ;
+                if ( left != right && nums[right] == y ) {
+                    String s = nums[i] + "," + nums[left] + "," + nums[right];
+                    if ( !set.contains(s) ) {
+                        ArrayList<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        res.add( list ) ;
+                        set.add(s);
+                    }
                 }
+                left++ ;
             }
-            len-- ;
-            while ( i < len && nums[i] == nums[i+1] ) i++ ;
-            len++ ;
         }
-        return result ;
+        return res ;
     }
 }
