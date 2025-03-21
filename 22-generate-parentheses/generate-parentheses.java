25574c57543fed8d761c1fde5ba980a72bf6f583
sanjay <sanjay.kr.on@gmail.com>
2025-03-21 14:03:28 +0530

    Time: 1 ms (82.87%) | Memory: 43.1 MB (76.53%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -4,20 +4,25 @@ import java.util.List;
 class Solution {
     public List<String> generateParenthesis(int n) {
         ArrayList<String> list = new ArrayList<>( (int)Math.pow( 2 , n ) );
-        fill( list , "" , n*2 , 0 , new char[2 * n] );
+        int maxLen = n * 2 ;
+        fill( list , new StringBuilder(maxLen) , maxLen , 0 , new char[maxLen] );
         return new ArrayList<>(list);
     }
-    void fill(ArrayList<String> result , String parenthesis , int maxLen , int stackPointer , char [] stack ) {
+    void fill(ArrayList<String> result , StringBuilder parenthesis , int maxLen , int stackPointer , char [] stack ) {
         if ( parenthesis.length()  == maxLen ) {
-            if ( stackPointer == 0 ) result.add( parenthesis );
+            if ( stackPointer == 0 ) result.add( parenthesis.toString() );
             return;
         }
         stack[stackPointer++] = '(' ;
-        fill( result , parenthesis+"(" , maxLen , stackPointer , stack  );
+        parenthesis.append('(') ;
+        fill( result , parenthesis , maxLen , stackPointer , stack  );
+        parenthesis.deleteCharAt(parenthesis.length()-1);
         stackPointer-- ;
         if ( stackPointer > 0 && stack[stackPointer-1] == '(' ) {
             stackPointer--;
-            fill( result , parenthesis+")" , maxLen , stackPointer , stack  );
+            parenthesis.append( ')' ) ;
+            fill( result , parenthesis , maxLen , stackPointer , stack  );
+            parenthesis.deleteCharAt(parenthesis.length()-1);
         }
     }
 }
