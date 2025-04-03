17027fd7e20d59ca872939bb24638be6589b41a2
sanjay <sanjay.kr.on@gmail.com>
2025-04-03 23:50:08 +0530

    0338-COUNTING-BITS Stats: Time: 0 ms (100%), Space: 46.6 MB (67.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,14 @@
 class Solution {
   public int[] countBits(int n) {
-    int[] result = new int[n+1];
-    for ( int i = 1 ; i <= n ; i++ ) {
-      result[i] = result[i>>1] + i % 2 ;
-    }
-    return result ;
+    int [] dp = new int[n+1];
+    recursiveCount(dp, 1, 1);
+    return dp;
+  }
+
+  public void recursiveCount(int [] dp, int index, int numsOnes ) {
+    if( index >= dp.length ) return;
+    dp[index] = numsOnes;
+    recursiveCount(dp, index*2, numsOnes);
+    recursiveCount(dp, index*2+1, numsOnes+1);
   }
 }
