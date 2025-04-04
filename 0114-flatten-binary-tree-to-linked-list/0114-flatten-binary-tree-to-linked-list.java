6e232fcc6dc03e1c545a47ff83b053c3514c0fc7
sanjay <sanjay.kr.on@gmail.com>
2025-04-05 04:52:10 +0530

    0114-FLATTEN-BINARY-TREE-TO-LINKED-LIST Stats: Time: 0 ms (100%), Space: 42.5 MB (26.35%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,17 +1,16 @@
 class Solution {
     public void flatten(TreeNode root) {
-        flattener( root , false , null );
+        flattener( root , null );
     }
-    TreeNode flattener(TreeNode node , boolean isRight , TreeNode child ) {
+    TreeNode flattener(TreeNode node , TreeNode child ) {
         if ( node == null ) return null;
         if ( node.left == null && node.right != null ) {
             node.left = node.right ;
             node.right = null ;
         }
-        flattener( node.left , false , ( node.right == null ) ? child : flattener( node.right , true , child ) );
-        if ( node.left == null && node.right == null ) {
+        flattener( node.left , ( node.right == null ) ? child : flattener( node.right , child ) );
+        if ( node.left == null && node.right == null ) 
             node.left = child ;
-        }
         if ( node.left != null) node.right = null ;
         TreeNode tmp = node.left;
         node.left = node.right;
