e97a1a16afb3794b265cac3c58404fbd5b411963
sanjay <sanjay.kr.on@gmail.com>
2025-04-04 23:05:36 +0530

    0883-CAR-FLEET Stats: Time: 11 ms (99.94%), Space: 57.8 MB (52.74%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,28 +1,16 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Stack;
-
 class Solution {
-    static class Pair {
-        int x, y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
     public int carFleet(int target, int[] position, int[] speed) {
-        Pair[] zipped = new Pair[position.length];
-        for ( int i = 0 ; i < position.length ; i++ ) {
-            zipped[i] = new Pair(position[i], speed[i]);
+        float[] time = new float[target];
+        for (int i = 0; i < speed.length; i++)
+            time[position[i]] = (float)(target-position[i])/speed[i];
+        int cnt = 0;
+        float max = 0;
+        for (int i = time.length - 1; i >= 0; i--) {
+            if (time[i] > max) {
+                cnt++;
+                max = time[i];
+            }
         }
-        Arrays.sort(zipped, (o1, o2) ->
-                Integer.compare(o2.x, o1.x)
-                );
-        Stack<Double> stack = new Stack<>();
-        for ( Pair pair : zipped ) {
-            double time = (double) ( target - pair.x ) / pair.y ;
-            if ( stack.isEmpty() || time > stack.peek() ) stack.push(time);
-        }
-        return stack.size() ;
+        return cnt;
     }
 }
