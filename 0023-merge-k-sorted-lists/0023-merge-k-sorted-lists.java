70c4b65f830f7a768bda62f64735a8522f1c66e4
sanjay <sanjay.kr.on@gmail.com>
2025-04-06 00:45:50 +0530

    0023-MERGE-K-SORTED-LISTS Stats: Time: 1 ms (99.92%), Space: 44.4 MB (63.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,25 +1,48 @@
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode cur = dummyHead;
-        int nullCount = 0;
-        for ( ListNode l : lists ) if ( l == null ) nullCount++;
-        while ( nullCount < lists.length ) {
-            ListNode smallest = null ;
-            int index = -1 ;
-            for ( int i = 0 ; i < lists.length ; i++ ) {
-                if ( lists[i] == null ) continue;
-                if ( smallest == null || smallest.val > lists[i].val ) {
-                    smallest = lists[i];
-                    index = i;
-                }
-            }
-            lists[index] = lists[index].next ;
-            if ( lists[index] == null ) nullCount++ ;
-            smallest.next = null ;
-            cur.next = smallest ;
-            cur = cur.next ;
+
+    static class pair {
+        int x;
+        int node ;
+        public pair(int x, int node) {
+            this.x = x;
+            this.node = node;
         }
-        return dummyHead.next ;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if ( lists.length == 0 ) return null;
+        if ( lists.length == 1 ) return lists[0];
+
+        int max = Integer.MIN_VALUE , min = Integer.MAX_VALUE;
+        for ( ListNode list : lists ) {
+            ListNode curr = list;
+            while ( curr != null ) {
+                max = Math.max(max, curr.val);
+                min = Math.min(min, curr.val);
+                curr = curr.next;
+            }
+        }
+
+        pair [] nodes = new pair[max - min + 1];
+        for ( ListNode list : lists ) {
+            ListNode curr = list;
+            while ( curr != null ) {
+                if ( nodes[curr.val-min] == null ) nodes[curr.val-min] = new pair(0, curr.val);
+                nodes[curr.val-min].x++ ;
+                curr = curr.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for ( pair node : nodes ) {
+            if ( node == null ) continue;
+            for ( int i = 0 ; i < node.x ; i++ ) {
+                curr.next = new ListNode(node.node);
+                curr = curr.next;
+            }
+        }
+        curr.next = null ;
+        return dummy.next;
     }
 }
