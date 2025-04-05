df012c89c53c1eff565c641bee1b48cee3b6e43f
sanjay <sanjay.kr.on@gmail.com>
2025-04-05 20:59:20 +0530

    0033-SEARCH-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 42 MB (67.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,20 +1,28 @@
-public class Solution {
+class Solution {
+
     public int search(int[] nums, int target) {
-        int rotation = 0 ,
-            len = nums.length ,
-            l = 0 , r = len - 1 , mid , lui ;
-        if ( len > 1 && nums[0] >= nums[len-1] ) {
-            do rotation++;
-            while (rotation < len && nums[rotation - 1] < nums[rotation]);
+        int offset = findMin(nums) ,
+            l = 0 , r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r-l) / 2;
+            int absIndex = (mid+offset)%nums.length;
+            if ( nums[absIndex] == target ) return absIndex ;
+            else if ( nums[absIndex] > target ) r = mid - 1;
+            else l = mid + 1;
         }
-        while ( l < r  ) {
-            mid = (l+r)/2;
-            lui = (mid + rotation)%len ;
-            if ( nums[lui] == target ) return lui ;
-            else if ( nums[lui] > target ) r = mid ;
-            else l = mid+1 ;
+        return -1 ;
+    }
+
+    int findMin(int[] nums) {
+        if ( nums.length == 1 || nums[0] < nums[nums.length-1]  ) return 0;
+        int left = 0, right = nums.length-1 ,
+            prior , mid = 0 ;
+        while ( left <= right ) {
+            prior = mid ;
+            mid = left + (right-left)/2;
+            if ( nums[mid] < nums[prior] ) right = mid - 1;
+            else left = mid + 1;
         }
-        lui = (((l + r) / 2) + rotation) % len;
-        return ( r == l && nums[lui] == target ) ? lui : -1 ;
+        return left ;
     }
 }
