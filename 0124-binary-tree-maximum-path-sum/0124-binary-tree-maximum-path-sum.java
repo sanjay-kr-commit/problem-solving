2e907b0cf8521bb556d4a475d7d024b354871232
sanjay <sanjay.kr.on@gmail.com>
2025-04-07 16:08:44 +0530

    0124-BINARY-TREE-MAXIMUM-PATH-SUM Stats: Time: 0 ms (100%), Space: 44.5 MB (49.42%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -1,33 +1,14 @@
 class Solution {
+    int ans = -20000;
+    public int maxPathSum2(TreeNode root) {
+        if( root == null ) return -20000;
+        int left = maxPathSum2(root.left);
+        int right = maxPathSum2(root.right);
+        ans = Math.max(ans, Math.max(left+root.val+right,Math.max(root.val,Math.max(left+root.val,right+root.val))));
+        return Math.max(root.val,Math.max(left+root.val,right+root.val));
+    }
     public int maxPathSum(TreeNode root) {
-        if ( root == null ) return 0 ;
-        int [] max = new int[1] ;
-        boolean allNegative = allNegative(root) ;
-        if ( allNegative ) return maxNodeVal( root ) ;
-        max[0] = Integer.MIN_VALUE;
-        traverse( root , max , 0 , 0 ) ;
-        return max[0] ;
-    }
-    
-    int maxNodeVal( TreeNode root ) {
-        if ( root == null ) return Integer.MIN_VALUE ;
-        return Math.max( root.val , Math.max( maxNodeVal( root.left ), maxNodeVal( root.right ) ) ) ;
-    }
-
-    boolean allNegative(TreeNode root) {
-        return root == null || ( root.val < 0 && allNegative(root.left) && allNegative(root.right) );
-    }
-
-    int traverse( TreeNode root, int [] max, int sum , int level ) {
-        if ( root == null ) return 0 ;
-        sum = Math.max( 0 , Math.max(sum + root.val, root.val)) ;
-        int left = traverse( root.left , max , sum , level + 1 ) ,
-            right = traverse( root.right , max , sum , level + 1 ) ;
-        max[0] = Math.max( max[0] , root.val ) ;
-        max[0] = Math.max( max[0] , sum ) ;
-        max[0] = Math.max( max[0] , left+sum ) ;
-        max[0] = Math.max( max[0] , right+sum ) ;
-        max[0] = Math.max( max[0] , right+left+root.val ) ;
-        return Math.max( 0 , Math.max( root.val , root.val + Math.max( left , right )) ) ;
+        maxPathSum2(root);
+        return ans;
     }
 }
