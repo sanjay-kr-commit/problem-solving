4d12bd9847a0de7b7b57ff8bdfe142056e279d5c
sanjay <sanjay.kr.on@gmail.com>
2025-04-08 10:54:02 +0530

    0078-SUBSETS Stats: Time: 1 ms (52.53%), Space: 42.7 MB (65.12%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,36 +1,26 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 class Solution {
+    private void addAllSubsetsUsingFirstN(int subSize, List<List<Integer>> subsets, int[] nums) {
+        int num = nums[subSize - 1];
+        int numSubsets = subsets.size();
+        for (int i = 0; i < numSubsets; i++) {
+            List<Integer> prevSubset = subsets.get(i);
+            List<Integer> subset = new ArrayList<>(prevSubset.size() + 1);
+            subset.addAll(prevSubset);
+            subset.add(num);
+            subsets.add(subset);
+        }
+    }
+
     public List<List<Integer>> subsets(int[] nums) {
-        ArrayList<List<Integer>> subsets = new ArrayList<>();
-        subsets(subsets,new ArrayList<>() , 0 , nums, new HashSet<>(), new boolean[nums.length] ) ;
-        return subsets ;
-    }
-
-    void subsets(ArrayList<List<Integer>> subsets , ArrayList<Integer> subset , int index , int [] nums , HashSet<String> used , boolean [] temp ) {
-        Arrays.fill( temp , false ) ;
-        for ( int i : subset ) temp[i] = true ;
-        StringBuilder st = new StringBuilder() ;
-        for ( int i = 0 ; i < temp.length ; i++ ) {
-            if ( temp[i] ) st.append( i ).append(",") ;
+        int n = nums.length;
+        List<List<Integer>> subsets = new ArrayList<>(1 << n);
+        subsets.add(new ArrayList<>(0));
+        for (int subSize = 1; subSize <= n; subSize++) {
+            addAllSubsetsUsingFirstN(subSize, subsets, nums);
         }
-        if (! used.contains( st.toString() ) ) {
-            used.add( st.toString() ) ;
-            ArrayList<Integer> tempSubset = new ArrayList<>() ;
-            for ( int i : subset ) {
-                tempSubset.add( nums[i] ) ;
-            }
-            subsets.add( tempSubset ) ;
-        }
-        if ( index == nums.length ) return;
-        ArrayList<Integer> path1 = new ArrayList<>(subset),
-                path2 = new ArrayList<>(subset);
-        path1.add(index++);
-        subsets(subsets , path1 , index , nums, used , temp) ;
-        subsets(subsets , path2 , index , nums, used, temp) ;
+        return subsets;
     }
-
 }
