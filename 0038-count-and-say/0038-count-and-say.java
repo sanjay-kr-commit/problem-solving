df8ce7eb46761bb59bc423f5d3503b049552b604
sanjay <sanjay.kr.on@gmail.com>
2025-04-18 16:58:47 +0530

    0038-COUNT-AND-SAY Stats: Time: 2 ms (92.38%), Space: 41.2 MB (79.02%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,17 +1,16 @@
 class Solution {
     public String countAndSay(int n) {
         if ( n == 1 ) return "1" ;
-        String say = "1" ;
-        for ( int k = 1 ; k < n ; k++ ) {
-            StringBuilder countedString = new StringBuilder() ;
-            int i = 0 , j = 0 ;
-            while ( i < say.length() ) {
-                while ( j < say.length() && say.charAt(i) == say.charAt(j) ) j++ ;
-                countedString.append(j-i).append(say.charAt(i));
-                i = j ;
-            }
-            say = countedString.toString() ;
+        String say = countAndSay( n-1 ) ;
+        StringBuilder compressedString = new StringBuilder() ;
+        // compress the string
+        int i = 0 , j = 0 ;
+        while ( i < say.length() ) {
+            while ( j < say.length() && say.charAt(j) == say.charAt(i) ) j++ ;
+            compressedString.append(j-i).append(say.charAt(i)) ;
+            i = j ;
         }
-        return say ;
+        // compression complete
+        return compressedString.toString() ;
     }
 }
