90a9b7c2f52282d610ab125460757cbe77ad0e4a
sanjay <sanjay.kr.on@gmail.com>
2025-04-19 15:25:53 +0530

    2699-COUNT-THE-NUMBER-OF-FAIR-PAIRS Stats: Time: 23 ms (100%), Space: 57.2 MB (84.51%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -2,24 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        long count = 0 ;
         Arrays.sort(nums);
-        int l = 0 , r = nums.length - 1;
-        while ( l < r ) {
-            long sum = nums[l] + nums[r];
-            if ( sum > upper ) r-- ;
-            else if ( sum < lower ) l++ ;
-            else break;
-        }
-        int lowerBound = l , upperBound = r ;
-        for ( int i = l ; i <= r ; i++ ) {
-            int k = i+1 ;
-            while ( k < r && nums[k]+nums[i] < lower ) k++ ;
-            int j = r ;
-            while ( j >= k && nums[j]+nums[i] > upper ) j-- ;
-            count += j-k+1 ;
-        }
-        return count ;
+        return count(nums, upper) - count(nums, lower - 1);
     }
-    
+
+    private long count(int[] nums, int target) {
+        long res = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] > target) right--;
+            else res += right - left++;
+        }
+        return res;
+    }
 }
