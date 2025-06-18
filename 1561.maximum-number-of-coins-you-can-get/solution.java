3a7d8325d46465de3fd7a518e35787fad579e15a
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2025-06-18 16:06:29 +0530

    1561.maximum-number-of-coins-you-can-get

---
+++ 1561.maximum-number-of-coins-you-can-get/solution.java
@@ -0,0 +1,15 @@
+// @leet start
+
+import java.util.Arrays;
+
+class Solution {
+  public int maxCoins(int[] piles) {
+    int collection = 0;
+    Arrays.sort(piles);
+    for (int i = 0, j = piles.length - 2, len = piles.length / 3; i < len; i++, j += 2) {
+      collection += piles[j];
+    }
+    return collection;
+  }
+}
+// @leet end
