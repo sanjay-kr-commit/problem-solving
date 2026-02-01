5781940c8c16d692f8e04e236a4d130833c6662f
sanjay <sanjay.kr.on@gmail.com>
2026-02-01 18:45:45 +0530

    0073-SET-MATRIX-ZEROES Stats: Time: 0 ms (100%), Space: 47.6 MB (25.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,27 +1,19 @@
 class Solution {
-  public void setZeroes(int[][] matrix) {
-    // mark rows and cols which need to be zeroed out
-    boolean [] array = new boolean[ matrix.length + matrix[0].length ] ;
-    for ( int i = 0 ; i < matrix.length ; i++ ) {
-      for ( int j = 0 ; j < matrix[0].length ; j++ ) {
-        // mark rows
-        array[i] = array[i] || matrix[i][j] == 0 ;
-        // mark rows
-        array[j+matrix.length] = array[j+matrix.length] || matrix[i][j] == 0 ;
-      }
+    public void setZeroes(int[][] matrix) {
+        int [] rows = new int[ matrix.length ] ,
+               cols = new int[ matrix[0].length ] ;
+        for ( int i = 0 ; i < matrix.length ; i++ ) {
+            for ( int j = 0 ; j < matrix[0].length ; j++ ) {
+                if ( matrix[i][j] != 0 ) continue ; 
+                rows[i] = 1 ;
+                cols[j] = 1 ;
+            }
+        }
+        for ( int i = 0 ; i < matrix.length ; i++ ) {
+            for ( int j = 0 ; j < matrix[0].length ; j++ ) {
+                if ( rows[i] != 1 && cols[j] != 1 ) continue ; 
+                matrix[i][j] = 0 ;
+            }
+        }
     }
-    // filling rows as zero 
-    for ( int i = 0 ; i < matrix.length ; i++ ) {
-      if ( !array[i] ) continue ;
-      for ( int j = 0 ; j < matrix[i].length ; j++ ) 
-        matrix[i][j] = 0 ;
-    }
-    // filling cols as zero 
-    for ( int i = matrix.length ; i < array.length ; i++ ) {
-      if ( !array[i] ) continue ;
-      int k = i - matrix.length ;
-      for ( int j = 0 ; j < matrix.length ; j++ ) 
-        matrix[j][k] = 0 ; 
-    }
-  }
 }
